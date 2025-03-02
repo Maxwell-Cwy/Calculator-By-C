19db47c43e98dbe2da7a1a9647ea746449865f29
cwy <2759438661@qq.com>
2025-03-02 11:06:21 +0800

    20240302-1106

---
+++ src/dfa.c
@@ -0,0 +1,96 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "dfa.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+char chbuf[100];
+int count = 0;
+int num = 0;
+
+int Isdigit(char ch) {
+    if (ch < 58 && ch>47) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+State DFA(char* input) {
+    if (Isdigit(*input)) {
+        chbuf[count] = *input;
+        count++;
+        return DIGIT;
+    }
+    if ((*input) == '(') {
+        chbuf[count] = *input;
+        count++;
+        return L;
+    }
+
+    if ((*input) == ')') {
+        chbuf[count] = *input;
+        count++;
+        return R;
+    }
+    if (*input == '+' || *input == '-' || *input == '*' || *input == '/' || *input == '\0') {
+        chbuf[count] = *input;
+        count++;
+        return OP;
+    }
+    else {
+        return ERROR;
+    }
+
+}
+
+
+void analays(char* p, State current) {
+    int i;
+    num=0;
+    for (i = 0; *p != '\0'; i++) {
+        current = DFA(p);
+        if (current == DIGIT && (!Isdigit(*(p + 1)))) {
+            printf("digit:%s\n", chbuf);
+            strcpy(token[num].value, chbuf);
+            token[num].id = 1;
+            num++;
+            memset(chbuf, 0, sizeof(chbuf));
+            count = 0;
+        }
+        if (current == OP) {
+            printf("OP:%s\n", chbuf);
+            strcpy(token[num].value, chbuf);
+            token[num].id = 2;
+            num++;
+            memset(chbuf, 0, sizeof(chbuf));
+            count = 0;
+        }
+        if (current == L) {
+            printf("L:%s\n", chbuf);
+            strcpy(token[num].value, chbuf);
+            token[num].id = 3;
+            num++;
+            memset(chbuf, 0, sizeof(chbuf));
+            count = 0;
+        }
+        if (current == R) {
+            printf("R:%s\n", chbuf);
+            strcpy(token[num].value, chbuf);
+            token[num].id = 4;
+            num++;
+            memset(chbuf, 0, sizeof(chbuf));
+            count = 0;
+        }
+        if (current == ERROR) {
+            printf("invalid char!\n");
+            memset(chbuf, 0, sizeof(chbuf));
+            count = 0;
+            exit(1);
+            break;
+        }
+        p++;
+    }
+}
---
+++ src/parse.c
@@ -0,0 +1,83 @@
+#include "parse.h"
+#include "dfa.h"
+#include <stdlib.h>
+#include <stdio.h>
+Token* t = token;
+
+int expr() {
+    int result_e, t1;
+    t1 = term();
+    result_e = expr_p(t1);
+    return result_e;
+}
+
+int expr_p(int temp) {//A(+B)(+C)
+    int result_ep, t1, t2;
+    if (t->id == 2 && t->value[0] == '+') {
+        t++;
+        t1 = term();
+        t2 = temp + t1;
+        result_ep = expr_p(t2);
+        return result_ep;
+    }
+    if (t->id == 2 && t->value[0] == '-') {
+        t++;
+        t1 = term();
+        t2 = temp - t1;
+        result_ep = expr_p(t2);
+        return result_ep;
+    }
+    else {
+        result_ep = temp;
+        return result_ep;
+    }
+}
+
+int term() {//A(*B)
+    int result_t, t1;
+    t1 = factor();
+    result_t = term_p(t1);
+    return result_t;
+}
+
+int term_p(int temp) {//A*B(*C)
+    int result_tp, t1;
+    if (t->id == 2 && t->value[0] == '*') {
+        t++;
+        t1 = temp * factor();
+        result_tp = term_p(t1);
+        return result_tp;
+    }
+
+    if (t->id == 2 && t->value[0] == '/') {
+        t++;
+        t1 = temp / factor();
+        result_tp = term_p(t1);
+        return result_tp;
+    }
+
+    else {
+        result_tp = temp;
+        return result_tp;
+    }
+}
+
+int factor() {
+    int result_f;
+    if (t->id == 1) {
+        result_f = atoi(t->value);//识别出数字字符
+        t++;
+        return result_f;
+    }
+    if (t->id == 3) {//识别出左括号字符
+        t++;
+        result_f = expr();//识别出表达式，指针+1;
+        if (t->id == 4) //识别出右括号字符
+            t++;
+        return result_f;
+    }
+    else {
+        printf("parse error!\n");
+        exit(1);
+    }
+}
